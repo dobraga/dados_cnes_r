60e57ad3451b3c5a47452a6cf8bbf0dc7f2317a2
dobraga <douglasmartinsbraga@gmail.com>
2018-09-16 17:15:35 -0300

    Adicionado exemplos de visualização com leaflet

--- exemplo.R
+++ exemplo.R
@@ -50,13 +50,47 @@ file.remove(dir(tempdir(),full.names = T))
 # EXEMPLOS LEAFLET #
 ####################
 
+estab.naomit = na.omit(estab %>% select(CNES,`NOME FANTASIA`,LATITUDE,LONGITUDE))
+
 # Distribuição de estabelecimentos de saude
 leaflet() %>% 
   addTiles('http://mt.google.com/vt/lyrs=m&x={x}&y={y}&z={z}') %>% 
   setView(lat = mean(estab$LATITUDE,na.rm=T),
           lng = mean(estab$LONGITUDE,na.rm=T),
-          zoom = 8) %>% 
-  addAwesomeMarkers(data = na.omit(estab %>% select(LATITUDE,LONGITUDE)),
+          zoom = 7) %>% 
+  addAwesomeMarkers(data = estab.naomit,
                     lat = ~LATITUDE,
                     lng = ~LONGITUDE,
-                    clusterOptions = markerClusterOptions())
+                    popup = paste("<p>CNES:",estab.naomit$CNES,"<br />",
+                                  "Nome fantasia:",estab.naomit$`NOME FANTASIA`,"</p>"),
+                    clusterOptions = markerClusterOptions()
+                    )
+
+# Estabelecimentos com mais de um funcionário
+
+qtd_prof = prof %>% 
+  group_by(CNES) %>% 
+  summarise(n=n())
+
+skimr::skim(qtd_prof) 
+
+sel.cnes = qtd_prof %>% 
+  filter(n>1)
+
+maiores.cnes = estab.naomit %>% 
+  inner_join(sel.cnes,by="CNES")
+
+leaflet() %>% 
+  addTiles('http://mt.google.com/vt/lyrs=m&x={x}&y={y}&z={z}') %>% 
+  setView(lat = mean(estab$LATITUDE,na.rm=T),
+          lng = mean(estab$LONGITUDE,na.rm=T),
+          zoom = 7) %>% 
+  addCircles(data = maiores.cnes,
+             lat = ~LATITUDE,
+             lng = ~LONGITUDE,
+             radius = ~n, 
+             stroke = F,
+             popup = paste("<p>CNES:",maiores.cnes$CNES,"<br />",
+                           "Nome fantasia:",maiores.cnes$`NOME FANTASIA`,"<br />",
+                           maiores.cnes$n,"funcionários","</p>"))
+
